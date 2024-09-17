895e413af216d2773b236897d5ff2cf16498d3ed
limonatasuyu <emredilek6@gmail.com>
2024-09-17 17:37:58 +0300

    cors options changed in backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const corsOptions: CorsOptions = {
+    origin: ['https://limonatasuyu.github.io'], // Specify the domain of your frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you're using cookies or authorization headers
+  };
+
+  app.enableCors(corsOptions);
   const port = process.env.PORT || 5000;
   await app.listen(port, '0.0.0.0');
 }
