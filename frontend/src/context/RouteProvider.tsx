25b9c7475ff477f0b51cc87912131634f3648b7a
limonatasuyu <emredilek6@gmail.com>
2024-09-18 09:26:46 +0300

    routing logic changed for github pages

--- frontend/src/context/RouteProvider.tsx
+++ frontend/src/context/RouteProvider.tsx
@@ -1,28 +1,42 @@
-import { useEffect, useState, createContext, useContext, useMemo, ReactNode } from "react";
+import {
+  useEffect,
+  useState,
+  createContext,
+  useContext,
+  useMemo,
+  ReactNode,
+} from "react";
 
-//@ts-expect-error path is needed in type
-const RoutesContext = createContext({ currentPath: "/", navigate: (path: string) => {} });
+const RoutesContext = createContext({
+  currentPath: "/",
+  //@ts-expect-error path is needed in type
+  navigate: (path: string) => {},
+});
 
 export default function RoutesProvider({ children }: { children: ReactNode }) {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  const [currentPath, setCurrentPath] = useState(
+    window.location.hash.slice(1) || "/"
+  );
 
   useEffect(() => {
-    const handlePopState = () => setCurrentPath(window.location.pathname);
-    window.addEventListener("popstate", handlePopState);
+    const handleHashChange = () =>
+      setCurrentPath(window.location.hash.slice(1) || "/");
+    window.addEventListener("hashchange", handleHashChange);
 
     return () => {
-      window.removeEventListener("popstate", handlePopState);
+      window.removeEventListener("hashchange", handleHashChange);
     };
   }, []);
 
   const navigate = (path: string) => {
-    window.history.pushState({}, "", path);
-    setCurrentPath(path);
+    window.location.hash = path;
   };
 
   const value = useMemo(() => ({ currentPath, navigate }), [currentPath]);
 
-  return <RoutesContext.Provider value={value}>{children}</RoutesContext.Provider>;
+  return (
+    <RoutesContext.Provider value={value}>{children}</RoutesContext.Provider>
+  );
 }
 
 export const useRoute = () => useContext(RoutesContext);
